a97593a0f686fc1a0dfdba245fd6b8ed973b118f
James Dillard <jamesedwarddillard@gmail.com>
2015-07-09 20:40:06 -0700

    added a thousands seperator

--- site.py
+++ site.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def nfp():
 	jobs = bls_reader(6, 2015)
-	return "Welcome to #nfpguesses. The most recent jobs number is {}.".format(jobs)
+	return "Welcome to #nfpguesses. The most recent jobs number is {:,}.".format(jobs)
 
 if __name__ == "__main__":
 	app.run()
