1474a2602db31c78af226b612581a3c895cdd590
James Dillard <jamesedwarddillard@gmail.com>
2015-07-09 17:48:56 -0700

    up running in flask

---
+++ bls_caller.py
@@ -0,0 +1,3 @@
+from bls_reader.bls_reader import bls_reader
+
+print bls_reader(6, 2015)
---
+++ site.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from bls_reader.bls_reader import bls_reader
+
+app = Flask(__name__)
+
+@app.route("/")
+def nfp():
+	jobs = bls_reader(6, 2015)
+	return "Welcome to #nfpguesses. The most recent jobs number is {}.".format(jobs)
+
+if __name__ == "__main__":
+	app.run()
