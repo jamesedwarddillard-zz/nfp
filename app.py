94bd9750cd9797183b8be2b492fcc1479d937470
James Dillard <jamesedwarddillard@gmail.com>
2015-07-31 22:59:51 -0700

    got started on the current report

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from bls_reader.bls_reader import bls_reader
 from written_months import written_months
+from current_report import current_report
 port = int(os.environ.get('Port', 5000))
 
 app = Flask(__name__)
@@ -12,11 +13,10 @@ def placeholder():
 
 @app.route("/current")
 def nfp():
-	mos = 4
-	year = 2014
+	mos, year = current_report()
 	month = written_months(mos)
 	jobs = bls_reader(mos, year)
-	return "Welcome to #nfpguesses. The most jobs number for {}, {} is {:,}.".format(month, year, jobs)
+	return "Welcome to #nfpguesses. The jobs number for {}, {} is {:,}.".format(month, year, jobs)
 
 if __name__ == "__main__":
 	app.run()
--- current_report.py
+++ current_report.py
@@ -1,5 +1,16 @@
+""" Identifies the current BLS report Month and Year based on the 
+current date and time"""
+
 import datetime
 
-today = datetime.datetime.now().date()
 
-print today
+
+def current_report():
+	current_date = datetime.datetime.now().date()
+	current_time = datetime.datetime.now().date()
+
+	mos = 7
+	year = 1986
+	return (mos, year)
+
+
