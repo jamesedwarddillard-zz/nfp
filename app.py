fec94678ea668d7ff1cf8f802a4a6c085fda6635
James Dillard <jamesedwarddillard@gmail.com>
2015-07-31 22:47:38 -0700

    created a written month function to identify written months

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from bls_reader.bls_reader import bls_reader
+from written_months import written_months
 port = int(os.environ.get('Port', 5000))
 
 app = Flask(__name__)
@@ -9,10 +10,13 @@ app = Flask(__name__)
 def placeholder():
 	return "Transmission of material in #nfpguess.es is embargoed."
 
-@app.route("/demo")
+@app.route("/current")
 def nfp():
-	jobs = bls_reader(6, 2015)
-	return "Welcome to #nfpguesses. The most recent jobs number is {:,}.".format(jobs)
+	mos = 4
+	year = 2014
+	month = written_months(mos)
+	jobs = bls_reader(mos, year)
+	return "Welcome to #nfpguesses. The most jobs number for {}, {} is {:,}.".format(month, year, jobs)
 
 if __name__ == "__main__":
 	app.run()
---
+++ written_months.py
@@ -0,0 +1,8 @@
+month_list = ['January', 'February', 'March',
+	'April', 'May', 'June', 'July', 'August', 'September',
+	'October', 'November', 'December']
+
+def written_months(mos):
+	number = mos - 1
+	month = month_list[number]
+	return month
