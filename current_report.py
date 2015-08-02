9f154d777261882b13d79522a7567a52b5a5c27f
James Dillard <jamesedwarddillard@gmail.com>
2015-08-01 22:00:53 -0700

    determined the most recent jobs report

--- current_report.py
+++ current_report.py
@@ -2,15 +2,20 @@
 current date and time"""
 
 import datetime
+from bls_reader.bls_reader import bls_api_request, bls_post_reader
+from bls_reader.bls_api import bls_api_series_id
 
 
 
 def current_report():
-	current_date = datetime.datetime.now().date()
-	current_time = datetime.datetime.now().date()
+	#identify the current year
+	year = datetime.datetime.now().year
 
-	mos = 7
-	year = 1986
+	#determine the number of reports so far this year by calling the bls api
+	response = bls_api_request(bls_api_series_id, year, year)
+	data = bls_post_reader(response)
+	mos = len(data)
+	
 	return (mos, year)
 
 
