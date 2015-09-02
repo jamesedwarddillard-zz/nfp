9bf83585c3075a5cd7dd278b0ffb3856edea9446
James Dillard <jamesedwarddillard@gmail.com>
2015-09-02 14:03:22 -0700

    tweet, user, created at time function made

--- twitter/twitter_search.py
+++ twitter/twitter_search.py
@@ -8,13 +8,29 @@ bearer_token = 'Bearer %s' % token
 payload = {'q': '#nfpguesses', 'count': 1}
 headers = {'Authorization': bearer_token}
 
-def main():
-	""" Main function """
+def twitter_search():
+	token = twitter_authorization.get_access_token()
+	bearer_token = 'Bearer %s' % token
+	payload = {'q': '#nfpguesses', 'count': 1}
+	headers = {'Authorization': bearer_token}
 	response = requests.get(SEARCH_ENDPOINT, params = payload, headers=headers)
 	response_json = json.loads(response.text)
-	tweet = response_json['statuses'][0]['text']
-	user = response_json['statuses'][0]['user']['screen_name']
-	print "{} - {}".format(tweet, user)
+	return response_json
+
+def single_response_parser(response):
+	tweet = response['statuses'][0]['text']
+	user = response['statuses'][0]['user']['screen_name']
+	created_at = response['statuses'][0]['created_at']
+	return tweet, user, created_at
+
+
+def main():
+	""" Main function """
+	response = twitter_search()
+	tweet, user, created_at = single_response_parser(response)
+	print tweet
+	print user
+	print created_at
 
 
 if __name__ == "__main__":
