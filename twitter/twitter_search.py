aab75413296b3fb82bd146926a2681b9f1dc50eb
James Dillard <jamesedwarddillard@gmail.com>
2015-09-02 09:50:38 -0700

    working twitter search for a single tweet

--- twitter/twitter_search.py
+++ twitter/twitter_search.py
@@ -5,15 +5,16 @@ import json
 
 token = twitter_authorization.get_access_token()
 bearer_token = 'Bearer %s' % token
-payload = {'q': '#nfpguesses', 'count': 10}
+payload = {'q': '#nfpguesses', 'count': 1}
 headers = {'Authorization': bearer_token}
 
 def main():
 	""" Main function """
 	response = requests.get(SEARCH_ENDPOINT, params = payload, headers=headers)
 	response_json = json.loads(response.text)
-	print response.status_code
-	print response_json
+	tweet = response_json['statuses'][0]['text']
+	user = response_json['statuses'][0]['user']['screen_name']
+	print "{} - {}".format(tweet, user)
 
 
 if __name__ == "__main__":
