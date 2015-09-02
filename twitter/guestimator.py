98fb11a221a30e4b86f91b90723fe6b81161c2ad
James Dillard <jamesedwarddillard@gmail.com>
2015-09-02 14:28:51 -0700

    sectioned off twitter seach

--- twitter/guestimator.py
+++ twitter/guestimator.py
@@ -1,1 +1,17 @@
 """ turns twitter searches into guesses"""
+
+from twitter_search import nfp_tweets
+
+response, tweets = nfp_tweets()
+
+def single_response_parser(response):
+	tweet = response['statuses'][0]['text']
+	user = response['statuses'][0]['user']['screen_name']
+	created_at = response['statuses'][0]['created_at']
+	return tweet, user, created_at
+
+def main():
+	print tweets
+
+if __name__ == '__main__':
+	main()
--- twitter/twitter_search.py
+++ twitter/twitter_search.py
@@ -3,36 +3,23 @@ from twitter_urls import *
 import requests
 import json
 
-token = twitter_authorization.get_access_token()
-bearer_token = 'Bearer %s' % token
-payload = {'q': '#nfpguesses', 'count': 1}
-headers = {'Authorization': bearer_token}
 
 def twitter_search():
 	token = twitter_authorization.get_access_token()
 	bearer_token = 'Bearer %s' % token
-	payload = {'q': '#nfpguesses', 'count': 1}
+	payload = {'q': '#nfpguesses'}
 	headers = {'Authorization': bearer_token}
 	response = requests.get(SEARCH_ENDPOINT, params = payload, headers=headers)
 	response_json = json.loads(response.text)
 	return response_json
 
-def single_response_parser(response):
-	tweet = response['statuses'][0]['text']
-	user = response['statuses'][0]['user']['screen_name']
-	created_at = response['statuses'][0]['created_at']
-	return tweet, user, created_at
+def number_of_tweets(response):
+	statuses = response['statuses']
+	tweets = len(statuses)
+	return tweets
 
-
-def main():
-	""" Main function """
+def nfp_tweets():
 	response = twitter_search()
-	tweet, user, created_at = single_response_parser(response)
-	print tweet
-	print user
-	print created_at
-
-
-if __name__ == "__main__":
-	main()
+	tweets = number_of_tweets(response)
+	return response, tweets
 
