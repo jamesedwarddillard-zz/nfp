b9d9983b70594e784700af2305cad08214b1dd78
James Dillard <jamesedwarddillard@gmail.com>
2015-10-23 01:54:02 -0700

    removing unneccssary files

--- twitter/tweet_class.py
+++
@@ -1,8 +0,0 @@
-""" A data class for NFP guess tweets """
-
-class Entries(object):
-	def __init__(self, tweet, user, utc_time):
-		self.tweet = tweet
-		self.user = user
-		self.utc_time = utc_time
-		self.guess = 'foo'
