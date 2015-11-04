6036e445fd5b2acfa731ba57c5fc8d3d367ddee0
James Dillard <jamesedwarddillard@gmail.com>
2015-11-03 20:31:48 -0800

    added the ability to display the most recent tweet

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from bls_reader.bls_reader import bls_reader
 from written_months import written_months
 from current_report import current_report
-from twitter.guestimator import response_generator, single_response_parser
+from twitter.guestimator import response_aggregator
+from twitter.twitter_search import nfp_tweets
+
 port = int(os.environ.get('Port', 5000))
 
 app = Flask(__name__)
@@ -26,12 +28,13 @@ def nfp():
 
 @app.route("/guesses")
 def guesses():
-	statuses, number_of_tweets = response_generator()
-	guesses = []
-	for i in range(0, number_of_tweets):
-		status = statuses[i]
-		entry = single_response_parser(status)
-		return entry[0], entry[1] 
+	entry_list = response_aggregator('#nfpguesses', '')
+	first_entry = entry_list[0]
+	user = first_entry['user']
+	guess = first_entry['guess']
+	tweet = first_entry['tweet']
+	time = first_entry['created_at']
+	return render_template('guesses.html', user = user, guess = guess, time = time, tweet = tweet)
 
 
 if __name__ == "__main__":
